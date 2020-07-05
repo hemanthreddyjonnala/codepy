ed417abd7f3eb85fe6724696bc2308c1c87de6d7
hemanthreddyjonnala <hemanthreddyjonnala@gmail.com>
2020-07-05 13:59:34 -0400

    add heoku host

--- agrofarming/settings.py
+++ agrofarming/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qpjsh2&3b3k78(_06x%vkv-kl4napo(5l7m448nsah&15%(m0c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['hjonnala.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['hjonnala.pythonanywhere.com', 'codepy.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
